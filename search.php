2d99b0847188aea23f0470fc2a4d623c44c3dd8f
aryayanroy <aryayanroy0@gmail.com>
2023-09-10 14:11:56 +0530

    initial commit

--- search.php
+++ search.php
@@ -5,12 +5,7 @@
         die();
     }
     include "config.php";
-    if($_SERVER["REQUEST_METHOD"]=="POST"){
-        $sql = $conn->prepare("INSERT INTO posts(user, content) VALUES (?, ?)");
-        $sql->bindParam(1, $_SESSION["id"], PDO::PARAM_INT);
-        $sql->bindParam(2, $_POST["post"], PDO::PARAM_STR);
-        $sql->execute();
-    }
+    
     $sql = $conn->prepare("SELECT name, type FROM users WHERE id = ?");
     $sql->bindParam(1, $_SESSION["id"], PDO::PARAM_INT);
     $sql->execute();
@@ -29,7 +24,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home | VyaparGuru</title>
+    <title>Search | VyaparGuru</title>
     <link rel="shortcut icon" href="assets/public/branding/favicon.ico" type="image/x-icon">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -69,62 +64,7 @@
     </header>
     <main class="flex-grow-1 bg-body-secondary">
         <article class="container-xxl py-3">
-            <h3>Suggested Volunteers</h3>
-            <?php
-                $sql = $conn->prepare("SELECT u.id, u.name AS user_name, u.profile_picture, s.name FROM users u JOIN user_skills us ON u.id = us.user JOIN skillset s ON us.skill = s.id WHERE us.skill IN (SELECT DISTINCT r.skill FROM requirements r WHERE r.user = ?)");
-                $sql->bindParam(1, $_SESSION["id"], PDO::PARAM_INT);
-                $sql->execute();
-                if($sql->rowCount()>0){
-            ?>
-            <div class="carousel-container">
-                <div class="owl-carousel owl-theme">
-            <?php
-                    while($users = $sql->fetch(PDO::FETCH_NUM)){
-                        if($users[2]==NULL){
-                            $users[2]="profile-picture-alternative";
-                        }
-            ?>
-                    <div class="item">
-                        <div class="ratio ratio-1x1 rounded-circle overflow-hidden mb-2"><img src="uploads/profile-pictures/<?php echo $users[2]; ?>.webp" alt="" class="img-thumbnail object-fit-cover"></div>
-                        <h5 class="text-center"><?php echo $users[1]; ?></h5>
-                        <h6 class="text-center mb-2"><?php echo $users[3]; ?></h6>
-                        <div class="d-flex justify-content-around">
-                            <a href="profile?id=<?php echo $users[0]; ?>" class="btn btn-success w-100">View Profile</a>
-                        </div>
-                    </div>
-            <?php
             
-                    }
-            ?>
-            </div>
-            </div>
-            <?php
-                }else{
-                    echo "No suggestions for now";
-                } 
-            ?>
-            <div class="mx-auto mt-5" style="max-width: 600px">
-                <form action="<?php echo $_SERVER["PHP_SELF"];?>" method="post">
-                    <div class="form-floating mb-2">
-                        <textarea id="post" name="post" class="form-control" placeholder="Write something...." autocomplete="off" style="height:100px; resize: none"></textarea>
-                        <label for="about">Write something...</label>
-                    </div>
-                    <button type="submit" class="btn btn-primary w-100">Post</button>
-                </form>
-                <ul class="list-group list-group-flush mt-3">
-                    <?php
-                        $sql = $conn->prepare("SELECT p.user, u.name, p.content, DATE_FORMAT(p.date, '%d %b %y') AS formatted_date FROM posts p JOIN users u ON p.user = u.id");
-                        $sql->execute();
-                        while($row = $sql->fetch(PDO::FETCH_NUM)){
-                            echo "<li class='list-group-item border-bottom mt-3'>
-                                <h6 class='pb-1 mb-0 border-bottom'><a href=profile?id=".$row[0]." class='link-dark text-decoration-none'>".$row[1]."</a></h6>
-                                <p class='py-3 mb-1 border-bottom'>".$row[2]."</p>
-                                <div class='text-end'>".$row[3]."</div>
-                            </li>";
-                        }
-                    ?>
-                </ul>
-            </div>
         </article>
     </main>
     <footer class="nav justify-content-around border-top sticky-bottom py-3 bg-white d-md-none">
@@ -152,40 +92,4 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.0/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
-<script>
-    $(document).ready(function(){
-        $(".owl-carousel").owlCarousel({
-            loop: true,
-            margin: 25,
-            stagePadding: 75,
-            autoplay:true,
-            nav: true,
-            autoplayTimeout:3000,
-            autoplayHoverPause:true,
-            responsive:{
-                0:{
-                    items: 1
-                },
-                375:{
-                    items: 2
-                },
-                576:{
-                    items: 3
-                },
-                768:{
-                    items: 4,
-                    mouseDrag: false
-                },
-                992:{
-                    items: 5,
-                    mouseDrag: false
-                },
-                1200:{
-                    items: 6,
-                    mouseDrag: false
-                }
-            }
-        })
-    })
-</script>
 </html>
